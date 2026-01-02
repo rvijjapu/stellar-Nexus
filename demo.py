428bdf547b30aea249f29975f912e418590678e9
rvijjapu <rajamanivsurya@gmail.com>
2026-01-02 10:26:23 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -59,13 +59,12 @@ st.set_page_config(
 st.markdown("""
 <style>
   .stApp {
-    background: linear-gradient(to bottom, rgba(255,255,255,0.92), rgba(240,248,255,0.88)),
-                url('https://raw.githubusercontent.com/rvijjapu/stellar-Nexus/main/image.jpg') no-repeat center center fixed;
-    background-size: cover;
-    color: #1e293b;
-    padding-top: 0.5rem;
-}
-    .header-container {
+        background: linear-gradient(to bottom, rgba(255,255,255,0.92), rgba(240,248,255,0.88)),
+                    url('https://raw.githubusercontent.com/rvijjapu/stellar-Nexus/main/image.jpg') no-repeat center center fixed;
+        background-size: cover;
+        color: #1e293b;
+        padding-top: 0.5rem;
+    } .header-container {
         background: rgba(255, 255, 255, 0.95);
         padding: 1.2rem 1.5rem;
         text-align: center;
